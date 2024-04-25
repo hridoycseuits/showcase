04cd93f3089a8c75f211f309f26e5946e13b3ff3
hridoycseuits <hridoycseuits@gmail.com>
2024-04-26 01:21:29 +0600

    hridoy's portfolio 101

---
+++ pages/About.php
@@ -0,0 +1,42 @@
+<div class="about-me container">
+
+      <div class="section-title">
+        <h2>About</h2>
+        <p>More About Me</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4" data-aos="fade-right">
+          <img src="assets/img/me.png" class="img-fluid" alt="">
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+          <h3>Mobile Application Developer</h3>
+          <h4>UI/UX &amp; Graphic Designer</h4>
+          <p class="fst-italic">
+            Experienced in Mobile Application Development, UI/UX Design.
+          </p>
+          <div class="row">
+            <div class="col-lg-6">
+              <ul>
+                <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>7<sup>th</sup> February</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>hridoycse.ezyro.com</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+8801829622102</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Dhaka, Bangladesh</span></li>
+              </ul>
+            </div>
+            <div class="col-lg-6">
+              <ul>
+                <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Bachelor of Science (BSc)</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Major:</strong> <span>Computer Science &amp; Engineering (CSE)</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>hridoyappdev@gmail.com</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+              </ul>
+            </div>
+          </div>
+          <p>
+            Developed mobile application, game, facebook instant game, google instant, playable ads, etc.<br/>Designed UI/UX, graphic design such as web banner, icon, social media design, etc..
+          </p>
+        </div>
+      </div>
+
+    </div>
